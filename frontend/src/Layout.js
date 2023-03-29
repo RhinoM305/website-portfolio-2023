3fc87a5a2972ac1ba4a6e83e0cda65348427127a
Ryan <morenoryan33@gmail.com>
2023-03-29 19:16:30 -0400

    small fixed changed track color

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -17,7 +17,7 @@ function Layout() {
     });
   }, []);
   return (
-    <div className="overflow-hidden scrollbar-none overflow-x: hidden;">
+    <div className="overflow-hidden overflow-x: hidden;">
       <Navbar />
       <Landing />
       <main className="w-full">
